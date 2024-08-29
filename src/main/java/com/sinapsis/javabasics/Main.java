58315e8e4335e640a2572ee46d5666d60a6ff045
Jonathan Calani Villarroel Calani <jonathan.c.villarroel.c@outlook.com>
2024-08-29 01:07:16 -0400

    Initial Commit

---
+++ src/main/java/com/sinapsis/javabasics/Main.java
@@ -0,0 +1,12 @@
+package com.sinapsis.javabasics;
+
+/**
+ *
+ * @author Jonathan C. Villarroel C.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
